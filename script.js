f66099d09a59e317c180ef25f1da25f5349a5e37
mishrishav <191943910+mishrishav@users.noreply.github.com>
2025-02-06 18:48:22 +0530

    -Modify code to display contact details dynamically

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
+// inserting contact details
+
+const contacts = {
+  email: "mishrishav@gmail.com",
+  phone: "(+91) 7004932616",
+};
+
+const contactSection = document.querySelector(".contactsSection");
+
+const phoneDiv = document.createElement("div");
+phoneDiv.innerHTML = `<p>Phone</p><span>${contacts.phone}</span>`;
+
+const emailDiv = document.createElement("div");
+emailDiv.innerHTML = `<p>Email</p><span>${contacts.email}</span>`;
+
+contactSection.appendChild(phoneDiv);
+contactSection.appendChild(emailDiv);
+
+// inserting skills details
+
 const skills = [
-    "HTML",
-    "CSS",
-    "Javascript",
-    "React.js",
-    "Node.js",
-    "Next.js",
-    "Java",
-  ];
-  
-  const skillsSection = document.querySelector(".skillsSection");
-  
-  skills.forEach((skill) => {
-    const span = document.createElement("span"); // Create a span element
-    span.textContent = skill; // Set text content (corrected)
-    span.classList.add("skillItem"); // Add a class
-    skillsSection.appendChild(span); // Append it to the section
-  });
-  
+  "HTML",
+  "CSS",
+  "Javascript",
+  "React.js",
+  "Node.js",
+  "Next.js",
+  "Java",
+];
+
+const skillsSection = document.querySelector(".skillsSection");
+
+skills.forEach((skill) => {
+  const span = document.createElement("span");
+  span.textContent = skill;
+  span.classList.add("skillItem");
+  skillsSection.appendChild(span);
+});
