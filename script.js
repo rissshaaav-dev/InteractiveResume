e2528aab8749ed3fbd4a38357bd660c27513afa3
mishrishav <191943910+mishrishav@users.noreply.github.com>
2025-02-06 19:31:30 +0530

    -Add styles and js to render sidearea dynamically

--- script.js
+++ script.js
@@ -2,16 +2,18 @@
 
 const contacts = {
   email: "mishrishav@gmail.com",
-  phone: "(+91) 7004932616",
+  phone: "(+91) 9876543210",
 };
 
 const contactSection = document.querySelector(".contactsSection");
 
 const phoneDiv = document.createElement("div");
-phoneDiv.innerHTML = `<p>Phone</p><span>${contacts.phone}</span>`;
+phoneDiv.innerHTML = `<p>Phone:</p><span><em>${contacts.phone}</em></span>`;
+phoneDiv.classList.add("contactItem");
 
 const emailDiv = document.createElement("div");
-emailDiv.innerHTML = `<p>Email</p><span>${contacts.email}</span>`;
+emailDiv.innerHTML = `<p>Email:</p><span><em>${contacts.email}</em></span>`;
+emailDiv.classList.add("contactItem");
 
 contactSection.appendChild(phoneDiv);
 contactSection.appendChild(emailDiv);
@@ -22,11 +24,13 @@ const educations = [
   {
     degree: "B.Tech CSE",
     school: "VIT Bhopal University",
+    location: "Bhopal, MP",
     year: "2021-2025",
   },
   {
     degree: "Senior Secondary",
     school: "Shanti Niketan Sr. Sec. School",
+    location: "Muzaffarpur, Bihar",
     year: "2018-2020",
   },
 ];
@@ -36,14 +40,14 @@ const educationSection = document.querySelector(".educationSection");
 educations.forEach((item) => {
   educationSection.insertAdjacentHTML(
     "beforeend",
-    `<div class="education-item">
+    `<div class="educationItem">
       <h3>${item.degree}</h3>
-      <p><strong>${item.school}</strong></p>
+      <p><em>${item.school}</em></p>
+      <p>${item.location}</p>
       <p>${item.year}</p>
     </div>`
   );
 });
-
 
 // inserting skills details
 
