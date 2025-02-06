fae3603a835bf058a337733d0c81815a8fd5728d
mishrishav <191943910+mishrishav@users.noreply.github.com>
2025-02-06 19:10:12 +0530

    -Modify code to add education details dynamically

--- script.js
+++ script.js
@@ -16,6 +16,35 @@ emailDiv.innerHTML = `<p>Email</p><span>${contacts.email}</span>`;
 contactSection.appendChild(phoneDiv);
 contactSection.appendChild(emailDiv);
 
+// inserting education details
+
+const educations = [
+  {
+    degree: "B.Tech CSE",
+    school: "VIT Bhopal University",
+    year: "2021-2025",
+  },
+  {
+    degree: "Senior Secondary",
+    school: "Shanti Niketan Sr. Sec. School",
+    year: "2018-2020",
+  },
+];
+
+const educationSection = document.querySelector(".educationSection");
+
+educations.forEach((item) => {
+  educationSection.insertAdjacentHTML(
+    "beforeend",
+    `<div class="education-item">
+      <h3>${item.degree}</h3>
+      <p><strong>${item.school}</strong></p>
+      <p>${item.year}</p>
+    </div>`
+  );
+});
+
+
 // inserting skills details
 
 const skills = [
